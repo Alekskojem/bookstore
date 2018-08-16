3b1ce54e8fbb47e2635e4ed0815cde28c410a7ad
alekskojem <alekskojem@gmail.com>
2018-08-16 18:13:06 +0300

    Add index and show html for preview books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
-
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+	
 	def index
+		@books = Book.all.order("created_at DESC")
+	end
+	
+	def show
 	end
 
 	def new
@@ -9,11 +14,21 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
+		
+		if @book.save
+			redirect_to root_path
+		else 
+			render'new'
+		end
 	end
 
 	private
 
 	def book_params
-	params.require(:book).permit(:title, :desription, :author) 
+	params.require(:book).permit(:title, :description, :author) 
+	end
+
+	def find_book
+		@book = Book.find(params[:id])
 	end
 end
